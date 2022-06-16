fd695cfa4ccfeedfbeaa877976ccd50ddd870349
Vadim Barsukov <v.bars@icloud.com>
2022-06-17 02:58:41 +0300

    word_optimize.py: on review

--- backend/word_optimize.py
+++ backend/word_optimize.py
@@ -1,24 +1,13 @@
-import time
+# import time
 from collections import Counter
 
 word_one: str = input().strip()
 word_two: str = input().strip()
 
-start_time = time.time()
-# use_index: set = set()
-
-# f = filter(
-#     lambda x: True if word_one[x[0]] != word_two[x[0]] else False, word_one
-# )
+# start_time = time.time()
 
 f = [val for ind, val in enumerate(word_one) if val != word_two[ind]]
 cnt = Counter(f)
-# print(list(f), type(f))
-# print(cnt)
-
-# cnt.subtract('B')
-# print(cnt)
-# print(cnt['B'])
 
 for ind, ch in enumerate(word_two):
     if ch == word_one[ind]:
@@ -31,4 +20,4 @@ for ind, ch in enumerate(word_two):
     else:
         print('absent')
 
-print("--- %s seconds ---" % (time.time() - start_time))
+# print("--- %s seconds ---" % (time.time() - start_time))
