117afb53ba94d52d0ac8354a89f94a9f4147f736
Vadim Barsukov <v.bars@icloud.com>
2022-06-18 17:43:47 +0300

    vacancy.py: debug v001

--- backend/vacancy.py
+++ backend/vacancy.py
@@ -1,25 +1,19 @@
-from typing import List, Tuple, Optional, Dict
-import time
-from operator import itemgetter
+from typing import List, Tuple, Dict
+# import time
 
 
 def foo(vacancy: Dict[str, int], arr: List[Tuple[str, str, int, int]]) -> List[str]:
-    print(vacancy)
-    print(arr)
+
     staff: List[str] = []
 
     for name, capcity in vacancy.items():
-        print('name, capcity:', name, capcity)
-        filtered_vacancy = list(filter(lambda x: x[1] == name, arr))
 
+        filtered_vacancy = list(filter(lambda x: x[1] == name, arr))
         rez: list = sorted(filtered_vacancy, key=lambda x: (-x[2], x[3]))
 
         end: int = min(len(rez), capcity)
         names = [x[0] for x in rez[:end]]
-        print('names=', names)
         staff = staff + names
-        print('rez=', rez)
-        print('staff=', staff)
 
     staff.sort()
 
@@ -40,15 +34,8 @@ def read_input() -> Tuple[dict, list]:
     return vacancy, arr
 
 
-def print_result(result: Optional[Tuple[int, int]]) -> None:
-    if result is None:
-        print(None)
-    else:
-        print(" ".join(map(str, result)))
-
-
 if __name__ == '__main__':
     vacancy, arr = read_input()
-    start_time = time.time()
+    # start_time = time.time()
     print('\n'.join(foo(vacancy, arr)))
-    print("--- %s seconds ---" % (time.time() - start_time))
+    # print("--- %s seconds ---" % (time.time() - start_time))
