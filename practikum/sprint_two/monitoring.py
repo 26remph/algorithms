f542ffb80ac5ab96f1a3c0c6635ea6cc73a1e2c4
Vadim Barsukov <v.bars@icloud.com>
2022-07-05 17:25:28 +0300

    sprint_two: monitoring on review

---
+++ practikum/sprint_two/monitoring.py
@@ -0,0 +1,22 @@
+from typing import Tuple, List, Dict
+
+
+def read_input() -> Tuple[int, int]:
+    n = int(input())
+    m = int(input())
+    return n, m
+
+
+n, m = read_input()
+t_matrix: Dict[int, List[str]] = {}
+for _ in range(n):
+    row: List[str] = list(map(str, input().split(' ')))
+    for m, row_val in enumerate(row):
+        if t_matrix.get(m):
+            t_matrix[m].append(row_val)
+        else:
+            t_matrix[m] = [row_val]
+
+for _, val in t_matrix.items():
+    print(' '.join(val))
+
