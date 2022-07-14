2bec21ba54bf2030a46b35555c917d6ac393740f
Vadim Barsukov <v.bars@icloud.com>
2022-07-14 12:35:57 +0300

    sprint_three: sequence write

--- practikum/sprint_three/example_recursion.py
+++ practikum/sprint_three/example_recursion.py
@@ -1,14 +1,14 @@
-def countDown(start, indent=1):
-    print('-'*indent, 'UP:', start)
+def countDown(start, indent=1, prefix='zero'):
+    print('-'*indent, 'UP:', start, prefix)
     if start == 0:
         # Здесь рекурсивный вызов 'countDown' прекратился, сначала
         #  печатается эта строчка, потом все, что было накоплено в стеке...
-        print('-'*indent, 'DOWN:', start)
+        print('-'*indent, 'DOWN:', start, prefix)
     else:
         # Рекурсивный вызов 'countDown'
-        countDown(start - 1, indent + 1)
-        # Вызов 'countDown' не дает функции print выполнится
+        countDown(start - 1, indent + 1, prefix='first')
+        countDown(start - 1, indent + 1, prefix='second')        # Вызов 'countDown' не дает функции print выполнится
         # и накапливает (откладывает) ее исполнение в стеке
-        print('-'*indent, 'DOWN:', start)
+        print('-'*indent, 'END:', start, prefix)
 
-countDown(5)
+countDown(3)
