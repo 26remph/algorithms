05b852c7b74e08a85c101b6c68a496b48abca855
Vadim Barsukov <v.bars@icloud.com>
2022-08-25 12:44:35 +0300

    ver 0.1.0v - made structure project

---
+++ devops_enr.py
@@ -0,0 +1,7 @@
+# yandex devops vacancy
+hash_p = 'SW52YWxpZCBjaGFyYWN0ZXIgaW4gaW5wdXQgc3RyZWFtLgo='
+hash_c = '0JjQt9GD0YfQuNGC0YwgQysrINC30LAgMjEg0LTQtdC90Yw='
+hash_ntlm = '71eebf819bfcf3c55c18f47673e25520'
+hash_test = 'MzU4NTA4MzIzNzA1MDQ=:19ofiY+ahBXhvkDsp0j2ww=='
+print('length', len(hash_test))
+
