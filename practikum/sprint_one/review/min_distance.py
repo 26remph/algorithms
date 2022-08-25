d1f1e0b26b0e6934a8001182b1e9009f99a4ed32
Vadim Barsukov <v.bars@icloud.com>
2022-08-25 12:58:48 +0300

    ver 0.1.2 - made structure project

---
+++ practikum/sprint_one/review/min_distance.py
@@ -0,0 +1,54 @@
+"""
+Ближайший ноль.
+
+Расчет минимальной дистанции
+"""
+# id 68997991
+from typing import List
+
+
+def calc_dist(street: List[int], min_dist: List[int], reverse=False) -> List[int]:
+    """Рассчитывает минимальное расстояние до нулевого значения,
+    в зависимости от направления обхода: прямой или обратный.
+    """
+    if reverse:
+        params = (len(street) - 1, -1, -1)
+    else:
+        params = (0, len(street), 1)
+
+    skip = True
+    zero_point: int = 0
+    for ind in range(*params):
+        if not skip and street[ind] > 0:
+            min_dist[ind] = min(abs(zero_point - ind), min_dist[ind])
+
+        if street[ind] == 0:
+            min_dist[ind] = 0
+            zero_point = ind
+            skip = False
+
+    return min_dist
+
+
+def get_dist(street: List[int]) -> List[int]:
+    """Алгоритм O(2n) сложности. Работает в два прохода. Прямой - для расчета
+     первого приближения минимальных расстояний к нулевым точкам массива
+     `street`. Обратный - уточняющий минимальные расстояния до нулевых точек
+     массиа `street`.
+     """
+    min_dist: list = [len(street)] * len(street)
+    min_dist = calc_dist(street, min_dist)
+    min_dist = calc_dist(street, min_dist, reverse=True)
+
+    return min_dist
+
+
+def read_input() -> List[int]:
+    _ = int(input())
+    street = list(map(int, input().strip().split()))
+    return street
+
+
+if __name__ == '__main__':
+    streets = read_input()
+    print(*get_dist(streets))
---
+++ practikum/sprint_one/review/sleight_of_hand.py
@@ -0,0 +1,28 @@
+"""
+Ловкость рук.
+Игра «Тренажёр для скоростной печати»
+"""
+# id 68997972
+from typing import List, Tuple
+from collections import Counter
+
+
+def get_sum(matrix: List[str], k: int) -> int:
+    """Рассчитывает число баллов, которое смогут заработать участники."""
+    cnt = Counter(matrix)
+    return len(list(filter(lambda x: x <= k * 2, cnt.values())))
+
+
+def read_input() -> Tuple[List[str], int]:
+    """Считывает ввод данных"""
+    k = int(input())
+    matrix: list = []
+    for _ in range(4):
+        row = [x for x in input().strip() if x != '.']
+        matrix = matrix + row
+    return matrix, k
+
+
+if __name__ == '__main__':
+    arr, key = read_input()
+    print(get_sum(arr, key))
