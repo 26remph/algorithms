3f6670aacb1b317843d06e6a02a02183dd383332
Vadim Barsukov <v.bars@icloud.com>
2022-09-03 17:08:08 +0300

    ver 0.2.11 - purchases.py-- on review ver 001

--- backend/autumn_backend/purchases.py
+++ backend/autumn_backend/purchases.py
@@ -2,28 +2,28 @@ import json
 import time
 
 
-def judge(record, _filter):
+def judge(record, sets):
 
-    if_1 = _filter['NAME_CONTAINS'].lower() in record['name'].lower()
-    if_2 = record['price'] >= int(_filter['PRICE_GREATER_THAN'])
-    if_3 = record['price'] <= int(_filter['PRICE_LESS_THAN'])
+    if_1 = sets['NAME_CONTAINS'].lower() in record['name'].lower()
+    if_2 = record['price'] >= int(sets['PRICE_GREATER_THAN'])
+    if_3 = record['price'] <= int(sets['PRICE_LESS_THAN'])
 
     date = time.strptime(record['date'], '%d.%m.%Y')
-    _after = time.strptime(_filter['DATE_AFTER'], '%d.%m.%Y')
-    _before = time.strptime(_filter['DATE_BEFORE'], '%d.%m.%Y')
+    _after = time.strptime(sets['DATE_AFTER'], '%d.%m.%Y')
+    _before = time.strptime(sets['DATE_BEFORE'], '%d.%m.%Y')
     if_4 = _after <= date <= _before
 
     is_true = all([if_1, if_2, if_3, if_4])
     if is_true:
-        _filter['match'] += 1
+        sets['match'] += 1
 
     return is_true
 
 
-def output(data: list, _filter):
+def output(data: list, sets):
 
-    data.sort(key=lambda x: (-judge(x, _filter), x['id']))
-    count = _filter['match']
+    data.sort(key=lambda x: (-judge(x, sets), x['id']))
+    count = sets['match']
 
     return json.dumps(data[:count]) if count > 0 else json.dumps({})
 
