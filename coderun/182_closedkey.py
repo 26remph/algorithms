2e23b7bc96606f1db133e2e570b978311b756664
Vadim Barsukov <v.bars@icloud.com>
2023-06-06 09:38:50 +0300

    coderun: 182 speed optimize

--- coderun/182_closedkey.py
+++ coderun/182_closedkey.py
@@ -1,3 +1,7 @@
+import random
+import time
+
+
 def gcd(a, b):
     a, b = max(a, b), min(a, b)
     while b:
@@ -5,30 +9,71 @@ def gcd(a, b):
     return a
 
 
-def main():
-    x, y = map(int, input().split())
+def test10ok(x, y):
+    # x, y = map(int, input().split())
 
-    keys = []
-    for i in range(x, y + 1):
-        if i % x == 0 and y % i == 0:
-            keys.append(i)
-
-    print(keys)
-    if not keys:
-        return 0
-    elif len(keys) == 1:
-        return 1
+    if x == y: return 1
 
     cnt = 0
-    for i in range(len(keys)-1):
-        for j in range(i+1, len(keys)):
-            print('-->', keys[i], keys[j])
-            if gcd(keys[i], keys[j]) == x and keys[i] * keys[j] / x == y:
-                print(keys[i], keys[j])
+    lim = int(x + (y - x) / 2)
+    for i in range(x, lim):
+        if i % x == 0 and y % i == 0:
+            j = y * x / i
+            if gcd(i, j) == x and i * j / x == y:
+                print(i, j)
                 cnt += 1
 
-    return cnt * 2
+    return cnt + 1 if cnt else cnt
+
+
+def main(x, y):
+    # x, y = map(int, input().split())
+
+    if x == y: return 1
+    if y % x != 0: return 0
+
+    t = time.time()
+    c = 2
+    for i in range(2, (y // x // 2) + 1):
+
+        if y % i == 0:
+            b = y // i
+            a = y * x / b
+
+            if a >= b:
+                break
+
+            if gcd(a, b) == x and a * b / x == y:
+                c += 2
+    print(time.time()-t, '(s)')
+
+    t = time.time()
+    cnt = 2
+    for i in range(2, (y // x // 2 + 1)):
+
+        a = x * i
+        b = y * x / a
+        if a >= b:
+            break
+
+        if gcd(a, b) == x and a * b / x == y:
+            cnt += 2
+    print(time.time() - t, '(s)')
+
+    assert cnt == c, f'cnt={cnt}, c={c}'
+
+    return cnt
 
 
 if __name__ == '__main__':
-    print(main())
+
+    for _ in range(1_000):
+        # x = random.randint(1, 100)
+        # y = random.randint(x, 100)
+        x, y = map(int, input().split())
+        t = time.time()
+        # print(test10ok(x, y))
+        # print('--->', time.time() - t)
+        t = time.time()
+        print(main(x, y), time.time() - t, '(s)')
+        # assert main(x, y) == test10ok(x, y), f'x={x}, y={y}'
