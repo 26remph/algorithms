7bb5c54dfe72988b06fb29ff0c0052f74a94078f
Vadim Barsukov <v.bars@icloud.com>
2023-08-03 13:32:05 +0300

    rebuild algo pack

---
+++ handbook/2.3. cycle/S.py
@@ -0,0 +1,20 @@
+def solution(left, right):
+
+    while left < right:
+        mid = (left + right) // 2
+        print(mid)
+        ans = input()
+        if ans == 'Угадал!':
+            break
+
+        if ans == 'Меньше':
+            right = mid
+        else:
+            left = mid + 1
+
+    print(left)
+
+
+if __name__ == '__main__':
+    left, right = 1, 1_000
+    solution(left, right)
---
+++ handbook/2.3. cycle/T.py
@@ -0,0 +1,35 @@
+import time
+
+
+def solution(blocks):
+
+    hash_prev = [0]
+    ans = -1
+    for i in range(len(blocks)):
+        b = blocks[i]
+        lim_m = (b - 255 - pow(256, 2)) // 65536 + 1
+
+        is_correct = False
+        cur_hash = []
+        for r in range(256):
+            for m in range(1, lim_m + 10):
+                for h_prev in hash_prev:
+                    h = (37 * (m + r + h_prev)) % 256
+                    if h < 100 and h == b - 256 * r - pow(256, 2) * m:
+                        cur_hash.append(h)
+                        is_correct = True
+                        print('i:', i, 'r:', r, 'm:', m, 'h:', h, 'lim_m:', lim_m, 'hash', hash_prev)
+        hash_prev = cur_hash[:]
+
+        if not is_correct:
+            return i
+
+    return ans
+
+
+if __name__ == '__main__':
+    N = int(input())
+    arr = [int(input()) for _ in range(N)]
+    # t = time.time()
+    print(solution(arr))
+    # print(time.time() - t)
