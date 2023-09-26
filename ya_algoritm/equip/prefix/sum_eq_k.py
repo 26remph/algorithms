5068c2296f409f54a924af5740d7f8845220875c
Vadim Barsukov <v.bars@icloud.com>
2023-09-26 21:32:40 +0300

    equip: prefix summ for arraay with possible negative integer

---
+++ ya_algoritm/equip/prefix/sum_eq_k.py
@@ -0,0 +1,17 @@
+import collections
+
+
+def subarray_sum(nums, k):
+    ans = 0
+    total = 0
+    prefix_summ = collections.defaultdict(int)
+    prefix_summ[0] = 1  # for case total equal k
+    for n in nums:
+        total += n
+        prefix = total - k
+        ans += prefix_summ.get(prefix, 0)
+        prefix[total] += 1
+
+    return ans
+
+
