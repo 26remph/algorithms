2b76ace5f0e28992bce2a72564417ed903d37362
Vadim Barsukov <v.bars@icloud.com>
2024-02-04 17:39:22 +0300

    module 3.5 input output operation (M, N) add-on

---
+++ handbook/35_io_json/M.py
@@ -0,0 +1,16 @@
+import json
+import sys
+
+fn = input()
+
+with open(fn, 'r+', encoding='UTF-8') as f:
+    d = json.load(f)
+    for line in sys.stdin:
+        key, val = line.split('==')
+        d: dict
+        d[key.strip()] = val.strip()
+
+    f.truncate()
+    f.seek(0)
+    json.dump(d, f, indent=4)
+
---
+++ handbook/35_io_json/N.py
@@ -0,0 +1,30 @@
+import json
+
+
+fn = input()
+with (
+    open(fn, 'rw+', encoding='UTF-8') as f_in,
+    open(input(), encoding='UTF-8') as f_upd
+):
+    out_dict = {}
+    for d in json.load(f_in):
+        key = d.get('name')
+        del d['name']
+        out_dict[key] = dict(d)
+
+    for d in json.load(f_upd):
+        key = d.get('name')
+        del d['name']
+        info = out_dict.get(key, {})
+
+        for k, v in d.items():
+            if k in info:
+                if str(v) > str(info[k]):
+                    info[k] = v
+            else:
+                info[k] = v
+
+        out_dict[key] = info
+
+with open(fn, 'w', encoding='UTF-8') as f:
+    json.dump(out_dict, f)
