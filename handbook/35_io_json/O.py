80de94843b8da647b716391258a64df79d885f83
Vadim Barsukov <v.bars@icloud.com>
2024-02-13 00:14:48 +0300

    handbook: module 3.5 task (O, P)

---
+++ handbook/35_io_json/O.py
@@ -0,0 +1,16 @@
+import json
+
+fn = 'o.json'
+tests = json.load(open(fn, encoding="UTF-8"))
+
+score = 0
+for group in tests:
+    points = group.get('points', 0)
+    for t in group.get('tests', []):
+        if t['pattern'] == input():
+            score += int(points) / len(group['tests'])
+
+print(int(score))
+
+
+
---
+++ handbook/35_io_json/P.py
@@ -0,0 +1,29 @@
+from sys import stdin
+
+s = input().strip()
+fn_lst = []
+for fn in stdin:
+    fn_lst.append(fn.rstrip())
+
+ans = []
+for fn in fn_lst:
+    q_arr = []
+    with open(fn, encoding="UTF-8") as f:
+        for line in f:
+            for q_str in line.rstrip().split():
+                if q_str:
+                    q_arr.append(q_str.lower())
+
+        if s.lower() in ' '.join(q_arr):
+            ans.append(fn)
+if ans:
+    print('\n'.join(ans))
+else:
+    print("404. Not Found")
+
+
+
+
+
+
+
