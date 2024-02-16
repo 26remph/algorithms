b486c3e9900d687dfb457907e6d4a38cfdb155eb
Vadim Barsukov <v.bars@icloud.com>
2024-02-16 20:10:34 +0300

    handbook: module 4.1 task (A, B)

---
+++ handbook/41_func/A.py
@@ -0,0 +1,9 @@
+def print_hello(param: str) -> str:
+    print(f'Hello, {param}')
+
+
+if __name__ == '__main__':
+    string = 'Yandex'
+    print_hello(string)
+    name = 'World'
+    print_hello(name)
---
+++ handbook/41_func/B.py
@@ -0,0 +1,34 @@
+def gcd_recurse(n1, n2):
+
+    if n1 < n2:
+        n1, n2 = n2, n1
+
+    if n2 == 0:
+        return n1
+
+    return gcd(n2, n1 % n2)
+
+
+def gcd(n1, n2):
+
+    if n1 < n2:
+        n1, n2 = n2, n1
+
+    while n2 != 0:
+        n1, n2 = n2, n1 % n2
+
+    return n1
+
+
+if __name__ == '__main__':
+    test = [(1071, 462, 21),
+            (0, 0, 0),
+            (1, 0, 1),
+            (462, 1071, 21),
+            (3, 2, 1),
+            (6, 3, 3), (12, 45, 3), (144, 96, 48)]
+    for param in test:
+        n1, n2, ans = param
+        assert gcd(n1, n2) == ans, f'{n1=},{n2=},{ans=}, {gcd(n1, n2)}'
+
+    print('pass')
