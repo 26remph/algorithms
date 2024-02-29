44c9bd297a13ee8f140b1754d828b1a989f22394
Vadim Barsukov <v.bars@icloud.com>
2024-02-29 03:33:27 +0300

    handbook: module 4.1 task (B)

--- handbook/43_recursion/B.py
+++ handbook/43_recursion/B.py
@@ -20,12 +20,4 @@ def recursive_digit_sum_ex(n: int, summa=0):
 if __name__ == '__main__':
     print(recursive_digit_sum(123))
     print(recursive_digit_sum(7321346))
-    # print(recursive_digit_sum(100))
-    #
-    # for _ in range(10_000_000):
-    #     summ = 0
-    #     num = random.randint(1, 1_000_000)
-    #     assert recursive_digit_sum(num) == sum(map(int, str(num))), num
 
-    for i in range(1_000_000_000):
-        assert int(i % 10) == i % 10 and int(i / 10) == i // 10, i
