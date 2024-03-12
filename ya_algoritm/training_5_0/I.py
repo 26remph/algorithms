b22a3090b6b3fc3e79218897ef2c8abecaad6245
Vadim Barsukov <v.bars@icloud.com>
2024-03-12 14:06:25 +0300

    training 5_0: (J) not accept

---
+++ ya_algoritm/training_5_0/I.py
@@ -0,0 +1,41 @@
+def main(y, holiday):
+    calendar = {
+        'January': 31, 'February': 28, 'March': 31, 'April': 30, 'May': 31, 'June': 30,
+        "July": 31, 'August': 31, 'September': 30, 'October': 31, 'November': 30, 'December': 31
+    }
+    if year % 400 == 0 or year % 4 == 0 and year % 100:
+        calendar['February'] = 29
+
+    day_of_week = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+    total = [0 for _ in range(len(day_of_week))]
+
+    pos = [k for k, v in day_of_week.items() if v == start_day][0]
+    for key, val in calendar.items():
+        for d in range(1, val + 1):
+            if (str(d), key) in holiday:
+                total[pos] -= 1
+            else:
+                total[pos] += 1
+
+            # print(f'{key=}, {pos=},{day_of_week[pos]}, {total=}, {(str(d), day_of_week[pos])=}')
+            pos += 1
+            pos = pos % 7
+
+    max_d = 0
+    min_d = 0
+    for i in range(len(total)):
+        if total[max_d] < total[i]:
+            max_d = i
+        if total[min_d] > total[i]:
+            min_d = i
+
+    # print(f'{total=}')
+    return f'{day_of_week[max_d]} {day_of_week[min_d]}'
+
+
+if __name__ == '__main__':
+    n = int(input())
+    year = int(input())
+    holiday = set(tuple(input().split()) for _ in range(n))
+    start_day = input()
+    print(main(year, holiday))
---
+++ ya_algoritm/training_5_0/J.py
@@ -0,0 +1,71 @@
+import random
+from sys import stdin
+from timeit import timeit
+
+
+def find_next_wdix():
+    print('find _ next _ w_idx')
+    pass
+
+
+def insert_word(word, type):
+    global w_idx
+
+    # scan row to end and insert word
+    start_x, start_y = w_idx[0], w_idx[1]
+    if not (doc[start_y]):
+        doc[start_y].append((0, len(word) * c, type))
+        w_idx = (start_x + 1, start_y)
+        print(f'init: {doc=}, {start_x=}, {start_y=}, {word=}, {w_idx=}, {coord_x=}')
+        return
+
+    if len(doc[start_y]) <= start_x:
+        last_offset = doc[start_y][-1][1]
+        if last_offset + len(word) * c <= w:
+            doc[start_y].append((last_offset, last_offset + len(word) * c, type))
+            w_idx = (start_x + 1, start_y)
+        else:
+            find_next_wdix()
+    else:
+        # present element
+        find_next_wdix()
+    print(f'init: {doc=}, {start_x=}, {start_y=}, {word=}, {w_idx=}, {coord_x=}')
+
+    # for y_ind, y_row in enumerate(doc, start_y):
+    #     if len(y_row) <= start_x:
+    #         y_row.append()
+    #     for x_ind, x_row in enumerate(y_row, start_x):
+
+
+def insert_img(img: dict):
+    if img['layout'] == 'embedded':
+        if int(img['height']) < h:
+            ...
+    pass
+
+
+if __name__ == '__main__':
+    w, h, c = map(int, input().split())
+    doc = [[] for _ in range(1)]
+    coord_x = [0 for _ in range(20)]
+    coord_y = [0 for _ in range(20)]
+    w_idx = (0, 0)
+    for words in stdin:
+        if words:
+            arr = words.split('(')
+            print(arr)
+            for el in arr:
+                if el.startswith('image') and el.find('layout') != -1:
+                    param = el.strip().rstrip(')')[6:].split()
+                    img = dict([tuple(p.split('=')) for p in param])
+                    print(img)
+                    insert_img(img)
+                elif el:
+                    print(el)
+                    insert_word(el.rstrip(), 'W')
+                else:
+                    print(f'{el=}, skip')
+        else:
+            print(f'{words=}, paragraph start')
+
+    # print(timeit('main()', number=2))
